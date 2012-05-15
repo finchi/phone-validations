90a60391b077e6a53eea5cf4daa2fbb38381e9e2
Christian Finck <christian@finck.at>
2012-05-15 14:07:48 +0200

    added rspec

--- lib/phone-validations.rb
+++ lib/phone-validations.rb
@@ -1,8 +1,2 @@
 require "phone-validations/version"
 require "phone-validations/austrian_phone_validations"
-
-module Phone
-  module Validations
-    # Your code goes here...
-  end
-end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,7 @@
+require 'rspec'
+require 'phone-validations'
+
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.formatter     = 'documentation'
+end
